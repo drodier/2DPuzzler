6f09e25e294c6c97a0f30c67a9ad65337aaa087e
drodier <davidrodier96@hotmail.com>
2023-03-25 13:29:32 -0400

    Add portal logic

--- Assets/Scripts/PortalController.cs
+++ Assets/Scripts/PortalController.cs
@@ -1,15 +1,17 @@
 using UnityEngine;
 
-public class Portal : MonoBehaviour
+public class PortalController : MonoBehaviour
 {
-    [SerializeField] private KeyCode portalKey = KeyCode.Space; // The key that the player needs to press to use the portal
+    [SerializeField] private bool isReturn = false;
 
-    private void OnTriggerStay2D(Collider2D other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player") && Input.GetKeyDown(portalKey))
+        if (other.CompareTag("Player"))
         {
-            Vector3 newPosition = other.transform.position + new Vector3(1000f, 0f, 0f);
-            other.transform.position = newPosition;
+            Vector3 positionOffset = new Vector3(isReturn ? -30f : 30f, 0f, 0f);
+            other.transform.position += positionOffset;
+            Camera.main.transform.position += positionOffset;
+            GameObject.Find("Player").GetComponent<CharacterController>().ChangeCurrentRoom(isReturn ? 0 : 1);
         }
     }
 }

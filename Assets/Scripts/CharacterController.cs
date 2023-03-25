cc2b29e6e907fbf36733574a7b49440c91ff1fff
drodier <davidrodier96@hotmail.com>
2023-03-25 12:56:58 -0400

    Ajouter le sprint

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -4,24 +4,26 @@ public class CharacterController : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 5f; // Character move speed
     [SerializeField] private float jumpForce = 10f; // Character jump force
+    [SerializeField] private float sprintSpeed = 2f;
     [SerializeField] private LayerMask groundLayer; // Layer mask for ground objects
     [SerializeField] private Transform groundCheck; // Transform object for checking if character is on ground
 
     private Rigidbody2D rb; // Character rigidbody component
-    private Animator anim; // Character animator component
+    //private Animator anim; // Character animator component
     private bool isGrounded; // Flag indicating if character is on ground
+    private bool isSprinting = false;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        anim = GetComponent<Animator>();
+        //anim = GetComponent<Animator>();
     }
 
     private void FixedUpdate()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
 
-        rb.velocity = new Vector2(horizontalInput * moveSpeed, rb.velocity.y);
+        rb.velocity = new Vector2(horizontalInput * moveSpeed * (isSprinting ? sprintSpeed : 1), rb.velocity.y);
 
         // Flip character sprite if moving in opposite direction
         if (horizontalInput > 0)
@@ -34,6 +36,7 @@ public class CharacterController : MonoBehaviour
     {
         // Check if character is on ground
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.1f, groundLayer);
+        isSprinting = isGrounded ? Input.GetKey(KeyCode.LeftShift) : false;
 
         // Character jump input
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
@@ -42,7 +45,7 @@ public class CharacterController : MonoBehaviour
         }
 
         // Update animator parameters
-        anim.SetFloat("Speed", Mathf.Abs(rb.velocity.x));
-        anim.SetBool("IsGrounded", isGrounded);
+        //anim.SetFloat("Speed", Mathf.Abs(rb.velocity.x));
+        //anim.SetBool("IsGrounded", isGrounded);
     }
 }

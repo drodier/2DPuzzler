1ac558410572a993a76074d826deb58d744fcbf7
gg12345678901 <128919596+gg12345678901@users.noreply.github.com>
2023-03-27 19:11:19 -0400

    modify laser mechanic to be grabable

--- Assets/Scripts/LaserBeamController.cs
+++ Assets/Scripts/LaserBeamController.cs
@@ -8,29 +8,32 @@ public class LaserBeamController : MonoBehaviour
     public LayerMask layerMask;
     public bool reflectRight = true;
 
-    public Vector2 reflectStartPosition; // Store the starting position of the current reflection
-    public bool isReflecting = false; // Track if the laser beam is currently reflecting
+    private Vector2 reflectStartPosition;
+    private bool isReflecting = false;
 
     private void Start()
     {
-        // Set up the Line Renderer
         laserBeam.startWidth = laserWidth;
         laserBeam.endWidth = laserWidth;
         laserBeam.positionCount = 2;
-        laserBeam.SetPosition(0, transform.position);
+        laserBeam.SetPosition(0, transform.position + transform.up * laserWidth / 2f);
     }
 
     private void Update()
     {
+        // Check the direction of the object and update the reflectRight variable accordingly
+        reflectRight = transform.localScale.x > 0;
+
+        // Update the starting position of the laser beam based on the position and rotation of the object
+        laserBeam.SetPosition(0, transform.position + transform.up * laserWidth / 2f);
+
         if (isReflecting)
         {
-            // If the laser beam is reflecting, calculate the reflection direction from the previous hit point
             Vector2 reflectDirection = Vector2.Reflect((reflectStartPosition - (Vector2)transform.position).normalized, reflectRight ? Vector2.left : Vector2.right);
             RaycastHit2D hit = Physics2D.Raycast(reflectStartPosition, reflectDirection, laserDistance, layerMask);
 
             if (hit)
             {
-                // If the raycast hit another Reflective object, continue reflecting the laser beam
                 if (hit.collider.CompareTag("Reflective"))
                 {
                     reflectStartPosition = hit.point;
@@ -38,26 +41,22 @@ public class LaserBeamController : MonoBehaviour
                 }
                 else
                 {
-                    // If the raycast hit a non-Reflective object, stop reflecting the laser beam
                     laserBeam.SetPosition(1, hit.point);
                     isReflecting = false;
                 }
             }
             else
             {
-                // If the raycast didn't hit anything, extend the laser beam to its maximum distance
                 laserBeam.SetPosition(1, reflectStartPosition + reflectDirection.normalized * laserDistance);
                 isReflecting = false;
             }
         }
         else
         {
-            // Cast a ray to determine the end point of the laser beam
-            RaycastHit2D hit = Physics2D.Raycast(transform.position, reflectRight ? Vector2.right : Vector2.left, laserDistance, layerMask);
+            RaycastHit2D hit = Physics2D.Raycast(transform.position + transform.up * laserWidth / 2f, reflectRight ? transform.right : -transform.right, laserDistance, layerMask);
 
             if (hit)
             {
-                // If the raycast hit a Reflective object, start reflecting the laser beam
                 if (hit.collider.CompareTag("Reflective"))
                 {
                     reflectStartPosition = hit.point;
@@ -66,14 +65,12 @@ public class LaserBeamController : MonoBehaviour
                 }
                 else
                 {
-                    // If the raycast hit a non-Reflective object, stop the laser beam
                     laserBeam.SetPosition(1, hit.point);
                 }
             }
             else
             {
-                // If the raycast didn't hit anything, extend the laser beam to its maximum distance
-                laserBeam.SetPosition(1, transform.position + (reflectRight ? Vector3.right : Vector3.left) * laserDistance);
+                laserBeam.SetPosition(1, transform.position + transform.up * laserWidth / 2f + (reflectRight ? transform.right : -transform.right) * laserDistance);
             }
         }
     }

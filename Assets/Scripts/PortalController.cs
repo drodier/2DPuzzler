d8dec4b1a6f5f0546aa9248c9407e181d2b5f377
drodier <davidrodier96@hotmail.com>
2023-03-28 18:05:40 -0400

    Modify the portal to keep it locked until puzzle is solved

--- Assets/Scripts/PortalController.cs
+++ Assets/Scripts/PortalController.cs
@@ -4,10 +4,21 @@ public class PortalController : MonoBehaviour
 {
     [SerializeField] private bool isReturn = false;
     private bool isPlayerInside = false; // flag to keep track of whether the player is inside the portal
+    private bool isLocked = false;
+
+    private Color lockedColor = new Color(.5f, .5f, .5f);
+    private Color unlockedColor = new Color(1, 1, 1);
+
+    void FixedUpdate()
+    {
+        isLocked = !GetComponent<ActivatableContent>().GetStatus();
+
+        GetComponent<SpriteRenderer>().color = isLocked ? lockedColor : unlockedColor;
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player") && !isPlayerInside)
+        if (other.CompareTag("Player") && !isPlayerInside && !isLocked)
         {
             isPlayerInside = true; // set flag to true
             Vector3 positionOffset = new Vector3(isReturn ? -30f : 30f, 0f, 0f);

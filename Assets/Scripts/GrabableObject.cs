2f7d959d91afdd571223a870de530948c9cb91d4
drodier <davidrodier96@hotmail.com>
2023-03-28 18:04:08 -0400

    Added the weight system to the grabable object

--- Assets/Scripts/GrabableObject.cs
+++ Assets/Scripts/GrabableObject.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class GrabableObject : MonoBehaviour
 {
     [SerializeField] private int pickupLockout = 10;
+    [SerializeField] private int weight = 1;
     private int timeHeld = 0;
     private bool isLockedOut = false;
     private CharacterController player;
@@ -47,5 +48,18 @@ public class GrabableObject : MonoBehaviour
     public bool IsLockedOut()
     {
         return isLockedOut;
+    }
+
+    public int GetWeight()
+    {
+        return weight;
+    }
+
+    public void ChangeWeight(int modification)
+    {
+        if(weight + modification < 1)
+            weight = 1;
+        else
+            weight += modification;
     }
 }

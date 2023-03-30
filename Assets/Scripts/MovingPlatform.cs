a4d2a3538f5c34995ee542f9594a717513a0ebb0
gg12345678901 <128919596+gg12345678901@users.noreply.github.com>
2023-03-30 18:22:19 -0400

    added moving platforms

---
+++ Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    public enum PlatformDirection { UpDown, LeftRight };  // define an enum for the platform direction
+    public PlatformDirection direction;                  // serialized field for the platform direction
+    public float distance = 5f;                          // serialized field for the distance the platform moves
+    public float speed = 2f;                             // speed of the platform movement
+
+    private Vector3 startPosition;                       // starting position of the platform
+    private Vector3 endPosition;                         // ending position of the platform
+    private float startTime;                             // time when the movement started
+    private float journeyLength;
+
+    void Start()
+    {
+        startPosition = transform.position;
+        if (direction == PlatformDirection.UpDown)
+        {
+            endPosition = new Vector3(startPosition.x, startPosition.y + distance, startPosition.z);
+        }
+        else
+        {
+            endPosition = new Vector3(startPosition.x + distance, startPosition.y, startPosition.z);
+        }
+        journeyLength = Vector3.Distance(startPosition, endPosition);
+    }
+
+        void Update()
+        {
+            float journeyTime = Time.time - startTime;
+            float fracJourney = Mathf.PingPong(journeyTime * speed, journeyLength) / journeyLength;
+            transform.position = Vector3.Lerp(startPosition, endPosition, fracJourney);
+            if (fracJourney == 1)
+            {
+                if (direction == PlatformDirection.UpDown)
+                {
+                    endPosition = startPosition;
+                    startPosition = transform.position;
+                }
+                else
+                {
+                    startPosition = endPosition;
+                    endPosition = new Vector3(startPosition.x + distance, startPosition.y, startPosition.z);
+                    journeyLength = Vector3.Distance(startPosition, endPosition);
+                }
+                startTime = Time.time;
+            }
+        }
+}

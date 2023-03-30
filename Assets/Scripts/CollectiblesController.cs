c0ce9834d2a9dd9763b8838537ba030c8c12dc80
gg12345678901 <128919596+gg12345678901@users.noreply.github.com>
2023-03-29 22:52:08 -0400

    fix UI_collectibles

--- Assets/Scripts/CollectiblesController.cs
+++ Assets/Scripts/CollectiblesController.cs
@@ -5,16 +5,19 @@ public class CollectiblesController : MonoBehaviour
 {
     public Image imagePrefab; // the image prefab to instantiate
     public Transform canvasTransform; // the canvas transform to add the image to
-    public Vector2 offset; // the offset from the top left corner of the canvas
+    public Vector2 offset = new Vector2(0, 0); // the offset from the top left corner of the canvas
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            // instantiate the image at the top left corner of the canvas with the specified offset
             Image image = Instantiate(imagePrefab, Vector3.zero, Quaternion.identity);
             image.transform.SetParent(canvasTransform, false);
-            image.rectTransform.anchoredPosition = offset;
+            image.rectTransform.anchorMin = new Vector2(0, 1); // set the anchor point to the top left corner
+            image.rectTransform.anchorMax = new Vector2(0, 1); // set the anchor point to the top left corner
+            image.rectTransform.pivot = new Vector2(0, 1); // set the pivot point to the top left corner
+            image.rectTransform.anchoredPosition = offset; // set the offset from the top left corner of the canvas
+
 
             Destroy(gameObject); // destroy the collectible object
         }

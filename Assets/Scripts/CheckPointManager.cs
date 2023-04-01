bb02ee8ed17475eaa606c15dab9d48c7dd000fa2
gg12345678901 <128919596+gg12345678901@users.noreply.github.com>
2023-04-01 16:53:25 -0400

    rearrange Level0 into a tutorial level

--- Assets/Scripts/CheckPointManager.cs
+++ Assets/Scripts/CheckPointManager.cs
@@ -4,78 +4,83 @@ using UnityEngine;
 
 public class CheckPointManager : MonoBehaviour
 {
-public Sprite activeSprite;
-public Sprite inactiveSprite;
-public bool isActive = false;
-public AudioClip checkpointSound;
-private SpriteRenderer spriteRenderer;
-private GameObject player;
-private AudioSource audioSource;
+    public Sprite activeSprite;
+    public Sprite inactiveSprite;
+    public bool isActive = false;
+    public AudioClip checkpointSound;
 
-void Start()
-{
-    spriteRenderer = GetComponent<SpriteRenderer>();
-    spriteRenderer.sprite = inactiveSprite;
+    private SpriteRenderer spriteRenderer;
+    private GameObject player;
+    private AudioSource audioSource;
 
-    // Find the player object in the scene
-    player = GameObject.FindGameObjectWithTag("Player");
-    if (player == null)
+    void Start()
     {
-        Debug.LogError("Player object not found in scene!");
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = inactiveSprite;
+
+        // Find the player object in the scene
+        player = GameObject.FindGameObjectWithTag("Player");
+        if (player == null)
+        {
+            Debug.LogError("Player object not found in scene!");
+        }
+
+        // Get the AudioSource component
+        audioSource = GetComponent<AudioSource>();
     }
 
-    // Get the AudioSource component
-    audioSource = GetComponent<AudioSource>();
-}
-
-void OnTriggerEnter2D(Collider2D other)
-{
-    if (other.CompareTag("Player"))
+    void OnTriggerEnter2D(Collider2D other)
     {
-        // Deactivate all other checkpoints in the scene
-        CheckPointManager[] checkpoints = FindObjectsOfType<CheckPointManager>();
-        foreach (CheckPointManager checkpoint in checkpoints)
+        if (other.CompareTag("Player"))
         {
-            if (checkpoint != this)
+            // Deactivate all other checkpoints in the scene
+            CheckPointManager[] checkpoints = FindObjectsOfType<CheckPointManager>();
+            foreach (CheckPointManager checkpoint in checkpoints)
             {
-                checkpoint.ResetCheckpoint();
+                if (checkpoint != this)
+                {
+                    checkpoint.ResetCheckpoint();
+                }
             }
-        }
 
-        isActive = true;
-        spriteRenderer.sprite = activeSprite;
-
-        // Play the checkpoint sound effect
-        if (audioSource != null && checkpointSound != null)
-        {
-            audioSource.PlayOneShot(checkpointSound);
-        }
-    }
-}
-
-public void ResetCheckpoint()
-{
-    isActive = false;
-    spriteRenderer.sprite = inactiveSprite;
-}
-
-public void RespawnPlayer()
-{
-    if (player != null)
-    {
-        // Find the active checkpoint and respawn the player at its position
-        CheckPointManager[] checkpoints = FindObjectsOfType<CheckPointManager>();
-        foreach (CheckPointManager checkpoint in checkpoints)
-        {
-            if (checkpoint.isActive)
+            if (!isActive) // Check if the checkpoint is not already active
             {
-                player.transform.position = checkpoint.transform.position;
-                break;
+                isActive = true;
+                spriteRenderer.sprite = activeSprite;
+
+                // Play the checkpoint sound effect
+                if (audioSource != null && checkpointSound != null)
+                {
+                    audioSource.PlayOneShot(checkpointSound);
+                }
             }
         }
     }
-    else
+
+    public void ResetCheckpoint()
     {
-        Debug.LogError("Player object not found in scene!");
+        isActive = false;
+        spriteRenderer.sprite = inactiveSprite;
     }
-}}
+
+    public void RespawnPlayer()
+    {
+        if (player != null)
+        {
+            // Find the active checkpoint and respawn the player at its position
+            CheckPointManager[] checkpoints = FindObjectsOfType<CheckPointManager>();
+            foreach (CheckPointManager checkpoint in checkpoints)
+            {
+                if (checkpoint.isActive)
+                {
+                    player.transform.position = checkpoint.transform.position;
+                    break;
+                }
+            }
+        }
+        else
+        {
+            Debug.LogError("Player object not found in scene!");
+        }
+    }
+}

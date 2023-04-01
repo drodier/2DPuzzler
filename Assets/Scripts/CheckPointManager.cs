de0241d2c5dd332a98f73bf1b6d33891a10a3e0c
gg12345678901 <128919596+gg12345678901@users.noreply.github.com>
2023-04-01 16:10:59 -0400

    added tutorials

--- Assets/Scripts/CheckPointManager.cs
+++ Assets/Scripts/CheckPointManager.cs
@@ -4,69 +4,78 @@ using UnityEngine;
 
 public class CheckPointManager : MonoBehaviour
 {
-    public Sprite activeSprite;
-    public Sprite inactiveSprite;
-    public bool isActive = false;
+public Sprite activeSprite;
+public Sprite inactiveSprite;
+public bool isActive = false;
+public AudioClip checkpointSound;
+private SpriteRenderer spriteRenderer;
+private GameObject player;
+private AudioSource audioSource;
 
-    private SpriteRenderer spriteRenderer;
-    private GameObject player;
+void Start()
+{
+    spriteRenderer = GetComponent<SpriteRenderer>();
+    spriteRenderer.sprite = inactiveSprite;
 
-    void Start()
+    // Find the player object in the scene
+    player = GameObject.FindGameObjectWithTag("Player");
+    if (player == null)
     {
-        spriteRenderer = GetComponent<SpriteRenderer>();
-        spriteRenderer.sprite = inactiveSprite;
-
-        // Find the player object in the scene
-        player = GameObject.FindGameObjectWithTag("Player");
-        if (player == null)
-        {
-            Debug.LogError("Player object not found in scene!");
-        }
+        Debug.LogError("Player object not found in scene!");
     }
 
-    void OnTriggerEnter2D(Collider2D other)
+    // Get the AudioSource component
+    audioSource = GetComponent<AudioSource>();
+}
+
+void OnTriggerEnter2D(Collider2D other)
+{
+    if (other.CompareTag("Player"))
     {
-        if (other.CompareTag("Player"))
+        // Deactivate all other checkpoints in the scene
+        CheckPointManager[] checkpoints = FindObjectsOfType<CheckPointManager>();
+        foreach (CheckPointManager checkpoint in checkpoints)
         {
-            // Deactivate all other checkpoints in the scene
-            CheckPointManager[] checkpoints = FindObjectsOfType<CheckPointManager>();
-            foreach (CheckPointManager checkpoint in checkpoints)
+            if (checkpoint != this)
             {
-                if (checkpoint != this)
-                {
-                    checkpoint.ResetCheckpoint();
-                }
-            }
-
-            isActive = true;
-            spriteRenderer.sprite = activeSprite;
-        }
-    }
-
-    public void ResetCheckpoint()
-    {
-        isActive = false;
-        spriteRenderer.sprite = inactiveSprite;
-    }
-
-    public void RespawnPlayer()
-    {
-        if (player != null)
-        {
-            // Find the active checkpoint and respawn the player at its position
-            CheckPointManager[] checkpoints = FindObjectsOfType<CheckPointManager>();
-            foreach (CheckPointManager checkpoint in checkpoints)
-            {
-                if (checkpoint.isActive)
-                {
-                    player.transform.position = checkpoint.transform.position;
-                    break;
-                }
+                checkpoint.ResetCheckpoint();
             }
         }
-        else
+
+        isActive = true;
+        spriteRenderer.sprite = activeSprite;
+
+        // Play the checkpoint sound effect
+        if (audioSource != null && checkpointSound != null)
         {
-            Debug.LogError("Player object not found in scene!");
+            audioSource.PlayOneShot(checkpointSound);
         }
     }
 }
+
+public void ResetCheckpoint()
+{
+    isActive = false;
+    spriteRenderer.sprite = inactiveSprite;
+}
+
+public void RespawnPlayer()
+{
+    if (player != null)
+    {
+        // Find the active checkpoint and respawn the player at its position
+        CheckPointManager[] checkpoints = FindObjectsOfType<CheckPointManager>();
+        foreach (CheckPointManager checkpoint in checkpoints)
+        {
+            if (checkpoint.isActive)
+            {
+                player.transform.position = checkpoint.transform.position;
+                break;
+            }
+        }
+    }
+    else
+    {
+        Debug.LogError("Player object not found in scene!");
+    }
+}}

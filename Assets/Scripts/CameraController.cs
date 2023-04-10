48123ab8a748a6cec3ea971df503d4b04a304327
drodier <davidrodier96@hotmail.com>
2023-04-10 19:44:26 -0400

    Script cleanup + Added comments

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -26,8 +26,10 @@ public class CameraController : MonoBehaviour
         // Check if the player object exists
         if (player != null)
         {
+            // Set the camera's desired position above the player's head
             desiredPosition = player.transform.position + new Vector3(player.transform.localScale.x, .5f, 0);
 
+            // Clamp the camera to not exit room borders
             desiredPosition = new Vector2(Mathf.Clamp(desiredPosition.x, minX, maxX), Mathf.Clamp(desiredPosition.y, minY, maxY));
         }
     }
@@ -37,7 +39,9 @@ public class CameraController : MonoBehaviour
         // Check if the player object exists
         if (player != null)
         {
+            // Set a direction vector with a clamped magnitude at maxCameraSpeed
             Vector2 direction = Vector2.ClampMagnitude(desiredPosition - new Vector2(transform.position.x, transform.position.y), maxCameraSpeed) * camSpeed * Time.deltaTime;
+            // Move camera towards desiredPosition
             transform.position += new Vector3(direction.x, direction.y, 0);
         }
     }

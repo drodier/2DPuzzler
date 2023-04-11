2909f4efc71981bd88f9662670b081c32d2bc326
gg12345678901 <128919596+gg12345678901@users.noreply.github.com>
2023-04-11 00:55:22 -0400

    add laser sound + lower volume when moving away

--- Assets/Scripts/LaserEmitterManager.cs
+++ Assets/Scripts/LaserEmitterManager.cs
@@ -6,15 +6,20 @@ public class LaserEmitterManager : MonoBehaviour
 {
     [SerializeField] private bool isActivated = true;
     [SerializeField] private float maxDistance = 10;
+    [SerializeField] private AudioClip laserSound;
 
     private Vector3 target;
     private LineRenderer laser;
+    private AudioSource audioSource;
 
     // Start is called before the first frame update
     void Start()
     {
         laser = GetComponent<LineRenderer>();
         laser.SetPosition(0, transform.position + new Vector3(0, 0, 2));
+        audioSource = GetComponent<AudioSource>();
+        audioSource.clip = laserSound;
+        audioSource.loop = true; // make sure the audio source is set to loop
     }
 
     // Update is called once per frame
@@ -49,6 +54,24 @@ public class LaserEmitterManager : MonoBehaviour
             }
 
             laser.SetPosition(1, target + new Vector3(0, 0, 2));
+
+            // calculate the distance between the player and the laser
+            float distanceToPlayer = Vector3.Distance(transform.position, GameObject.FindGameObjectWithTag("Player").transform.position);
+
+            // set the volume of the audio source based on the distance to the player
+            audioSource.volume = 0.8f / (distanceToPlayer * 15);
+
+            if(!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
+        else
+        {
+            if(audioSource.isPlaying)
+            {
+                audioSource.Stop();
+            }
         }
     }
 

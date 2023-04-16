e7bdfa0525f975b4db988710c805b159585b4a45
drodier <davidrodier96@hotmail.com>
2023-04-16 13:56:30 -0400

    Add dropdown platforms

---
+++ Assets/Scripts/DropDownPlatform.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropDownPlatform : MonoBehaviour
+{
+    private bool isHoldingDown = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        isHoldingDown = Input.GetKey(KeyCode.S);
+    }
+
+    void FixedUpdate()
+    {
+        PlatformEffector2D effector = GetComponent<PlatformEffector2D>();
+
+        if(isHoldingDown)
+            effector.rotationalOffset = 180;
+        else
+            effector.rotationalOffset = 0;
+    }
+}
